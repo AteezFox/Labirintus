89c2173701277d0281500dc46c1f69fae2eb8f62
Mr. Lowfast <74541829+AteezFox@users.noreply.github.com>
2024-09-20 11:25:02 +0200

    Delete Labirintus__ directory

--- Labirintus__/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.labirintus__ {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.labirintus__ to javafx.fxml;
-    exports com.example.labirintus__;
-}
